54e3ae9ac2b97ef8a9dc3ec00549c31a0eec5d2b
Konrad Chmiel <chmielagh@gmail.com>
2016-04-25 23:25:37 +0200

    lab7

---
+++ lab7/WebApplication1/DAL/StoreInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.DAL
+{
+    public class StoreInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<StoreContext>
+    {
+        protected override void Seed(StoreContext context)
+        {
+            var books = new List<Book>
+            {
+                new Book() { Id=1, ISBN="sksz", Title="Smętarz zwierząt" },
+                new Book() { Id=2, ISBN="skc", Title="Cujo" },
+                new Book() { Id=3, ISBN="slm", Title="Millenium" }
+
+            };
+            context.Books.AddRange(books);
+            context.SaveChanges();
+
+            var authors = new List<Author>()
+            {
+                new Author() { Id=1, Name="Stephan", Surname="King" },
+                new Author() { Id=2, Name="Stieg", Surname="Larssen" }
+            };
+            context.Authors.AddRange(authors);
+            context.SaveChanges();
+
+        }
+    }
+}
---
+++ lab7/WebApplication1/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        int PageCount { get; set; }
+    }
+}
